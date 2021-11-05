305a3b9d3c9d907681da04a4898ce98ebd30c4f3
brain <shahriarmehedi95@gmail.com>
2021-11-05 19:07:23 +0600

    props

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,30 @@
+import React from 'react';
 import './App.css';
+import Header from './Header'
+import Addcontact from './Addcontact'
+import Contactlist from './Contactlist'
 
 function App() {
-  return <div>Hello Shahriar</div>
+  const contacts=[
+    {
+      id:"1",
+      name:"shahriar",
+      email:"shahriarmehedi94@gmail.com",
+    },
+    {
+      id:"2",
+      name:"mehedi",
+      email:"shahriar@gmail.com",
+    }
+  ];
+  return (
+    <div className="ui container">
+      <Header />    
+      <Addcontact />
+      <Contactlist />
+    </div>
+  );
+  
 }
 
 export default App;
---
+++ src/components/Contactlist.js
@@ -0,0 +1,11 @@
+ import React from "react";
+
+ const Contactlist = () => {
+     return(
+         <div className="ui celled list">
+            Contact List
+         </div>
+     );
+ };
+
+ export default Contactlist
