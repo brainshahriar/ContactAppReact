612b7562047cb0840146532098e5646fb6e1f91f
brain <shahriarmehedi95@gmail.com>
2021-11-05 20:29:36 +0600

    render

--- src/components/Contactlist.js
+++ src/components/Contactlist.js
@@ -1,19 +1,12 @@
- import React from "react";
+import React from "react";
+import Contactcard from "./Contactcard";
 
  const Contactlist = (props) => {
      console.log(props);
 
      const renderContactlist=props.contacts.map((contact) => {
          return(
-             <div className="item">
-                 <div className="content">
-                    <div className="header">
-                        {contact.name}
-                    </div>
-                    <div>{contact.email}</div>
-                 </div>
-                 <i className="trash alternate outline icon"></i>
-             </div>
+            <Contactcard contact={contact}></Contactcard>
          );
      });
      return <div className="ui celled list">
@@ -22,4 +15,4 @@
      
  };
 
- export default Contactlist
+ export default Contactlist;
